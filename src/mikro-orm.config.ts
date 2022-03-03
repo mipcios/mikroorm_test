20e84c7448af2bb0801ee8ed93d4c3b136739d96
Miguel Perez <miguelytperez94@gmail.com>
2022-03-03 10:20:49 +0100

    wip

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,26 +1,23 @@
 import { MikroOrmModuleOptions as Options } from '@mikro-orm/nestjs';
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
-import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
-import { LoadStrategy } from '@mikro-orm/core';
+import { Component } from './component/component.entity';
+import { Step } from './step/step.entity';
+import { UserAction } from './user-action/user-action.entity';
 
 const config: Options = {
+  entities: [
+    UserAction,
+    Step,
+    Component,
+  ],
   type: 'mariadb',
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: 'root',
   dbName: 'nestjsrealworld',
-  entities: ['dist/**/*.entity.js'],
-  entitiesTs: ['src/**/*.entity.ts'],
   debug: true,
-  loadStrategy: LoadStrategy.JOINED,
   highlighter: new SqlHighlighter(),
-  metadataProvider: TsMorphMetadataProvider,
-  registerRequestContext: false,
-  migrations: {
-    path: 'dist/migrations',
-    pathTs: 'src/migrations',
-  },
 };
 
 export default config;
